92134243e08bf4c3864c3a7695df2c15ad82a0df
Gady359 <gady.peters@gmail.com>
2024-05-06 16:15:22 -0300

    add functions

--- Sistema Bancário.py
+++ Sistema Bancário.py
@@ -1,52 +1,88 @@
-menu="""
+def menu():
+    menu="""
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
+    """
+    return input(menu)
 
-"""
+def deposito(saldo,valor,extrato, /):
+    print("Deposito")
 
-saldo = 0
-limite = 500
-extrato = ""
-num_saq = 0
-lim_saq = 3
-
-while True:
-
-    opcao = input(menu)
-
-    if opcao == "d":
-        print("Deposito")
-        deposito = float(input("Quanto deseja depositar?"))
-
-        if deposito > 0:
-            saldo += deposito
-            extrato += f"Deposito: R$ {deposito:.2f}\n"
-        else:
-            print("Valor invalido")
-
-    elif opcao == "s":
-        print("Saque")
-        saque = float(input("Quanto deseja sacar?"))
-
-        if saque > 0 and num_saq < lim_saq:
-            if saque <= limite and saque <= saldo:
-                saldo -= saque
-                num_saq += 1
-                extrato += f"Saque: R$ {saque:.2f}\n"
-            else:
-                print("Saque excede limite ou saldo insuficiente")
-        else:
-            print("Valor de saque inválido ou limite de saques excedido")
-
-    elif opcao == "e":
-        print("Extrato")
-        print(extrato)
-
-    elif opcao == "q":
-        break
-
+    if valor > 0:
+        saldo += valor
+        extrato += f"Deposito: R$ {valor:.2f}\n"
+        print("Sucesso")
     else:
-        print("Opcao invalida")
+        print("Valor invalido")
+
+    return saldo, extrato
+
+def saque(*,saldo,valor,extrato, lim_saq, limite,num_saq):
+    print("Saque")
+    if valor > 0 and num_saq < lim_saq:
+                if valor <= limite and valor <= saldo:
+                    saldo -= valor
+                    num_saq += 1
+                    extrato += f"Saque: R$ {valor:.2f}\n"
+                    print("Sucesso")
+                else:
+                    print("Saque excede limite ou saldo insuficiente")
+    else:
+        print("Valor de saque inválido ou limite de saques excedido")
+
+    return saldo, extrato
+
+
+def Mostrar_extrato(saldo, /, *,extrato):
+     print("Extrato")
+     print("Nao foi realizado nehuma movimentacao" if not extrato else extrato)
+     print(f"\nSaldo:\t\tR$ {saldo:.2f}")
+     
+
+def main():
+    saldo = 0
+    limite = 500
+    extrato =""
+    num_saq = 0
+    lim_saq = 3
+    contas=[]
+    clientes=[]
+
+    agencia="0001"
+
+    while True:
+
+        opcao = menu()
+
+        if opcao == "d":
+            valor = float(input("Quanto deseja depositar?"))
+            saldo,extrato=deposito(saldo,valor,extrato)
+
+        elif opcao == "s":
+            
+            valor = float(input("Quanto deseja sacar?"))
+
+            saldo,extrato =saque(
+                 saldo=saldo,
+                 valor=valor,
+                 extrato=extrato,
+                 limite=limite,
+                 num_saq=num_saq,
+                 lim_saq=lim_saq,
+            )
+
+            
+
+        elif opcao == "e":
+            Mostrar_extrato(saldo,extrato=extrato)
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("Opcao invalida")
+
+main()
