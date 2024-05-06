3958bbbe7f1a87af0a027c5d88e490e41ae04245
Gady359 <gady.peters@gmail.com>
2024-05-06 16:43:22 -0300

    clientes e contas

--- Sistema Bancário.py
+++ Sistema Bancário.py
@@ -5,6 +5,9 @@ def menu():
     [s] Sacar
     [e] Extrato
     [q] Sair
+    [nu] Novo Cliente
+    [nc] Nova Conta
+    [lc] Listas Contas
     """
     return input(menu)
 
@@ -41,6 +44,47 @@ def Mostrar_extrato(saldo, /, *,extrato):
      print("Nao foi realizado nehuma movimentacao" if not extrato else extrato)
      print(f"\nSaldo:\t\tR$ {saldo:.2f}")
      
+def criar_cliente(clientes):
+     cpf= input("Informe seu CPF(somente numero): ")
+     cliente =filtrar_cliente(cpf,clientes)
+
+     if cliente:
+        print("Cliente ja existente com esse CPF!!!")
+        return
+     
+     nome=input("Nome Completo:")
+     data_nasc=input("Data de nascimento(dd-aa-aaaa): ")
+     endereco=input("Endereco (logradouro, num - bairro - cidade/sigla estado): ")
+
+     clientes.append({"nome": nome, "data_nasc": data_nasc, "cpf":cpf, "endereco": endereco})
+
+     print("Sucesso")
+        
+def filtrar_cliente(cpf, clientes):
+    clientes_filtered=[cliente for cliente in clientes if cliente["cpf"]== cpf]
+    return clientes_filtered[0] if clientes_filtered else None
+
+def criar_conta(agencia,num_conta,clientes):
+    cpf=input("informe o CPF: ")
+    cliente =filtrar_cliente(cpf,clientes)
+
+    if cliente:
+        print("Conta criada com sucesso")
+        return {"agencia": agencia,"num_conta":num_conta,"cliente":cliente}
+    
+    print("Cliente nao encontrado!")
+
+def listar_contas(contas):
+    for conta in contas:
+        linha=f"""\
+            Agencia:\t{conta['agencia']}
+            C/C:\t{conta['num_conta']}
+            Titular:\t{conta['cliente']['nome']}
+            """
+        print("="* 20)
+        print(linha)
+
+
 
 def main():
     saldo = 0
@@ -74,10 +118,21 @@ def main():
                  lim_saq=lim_saq,
             )
 
-            
-
         elif opcao == "e":
             Mostrar_extrato(saldo,extrato=extrato)
+
+        elif opcao=="nu":
+            criar_cliente(clientes)
+
+        elif opcao=="nc":
+            num_conta =len(contas)+1
+            conta = criar_conta(agencia,num_conta,clientes)
+
+            if conta:
+                contas.append(conta)
+
+        elif opcao=="lc":
+            listar_contas(contas)
 
         elif opcao == "q":
             break
